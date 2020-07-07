4bd2af4038e0915ef30aed3d99cb64e0cd363f76
ahamedfofana <ahamedfofana12@gmail.com>
2020-07-07 01:37:47 -0400

    Updated MMcorpus.serialize

--- MediaCloud.py
+++ MediaCloud.py
@@ -22,7 +22,7 @@ import jsonpickle
 import pyLDAvis
 import pyLDAvis.gensim
 import warnings
-
+import random
 
 nlp = spacy.load('en')
 
@@ -91,7 +91,9 @@ def story_runner(storiez):
 def text_reader():
     with open('black-lives-matter-all-story-urls-20200621214756.csv') as csv_file:
         list_rows = []
-        csv_reader = csv.reader(csv_file, delimiter=',')
+        csv_reader = list(csv.reader(csv_file, delimiter=','))
+        random.shuffle(csv_reader)
+        print(csv_reader)
         line_count = 0
         for row in csv_reader:
             if line_count == 0:
@@ -115,7 +117,7 @@ def article_reader(list):
             continue
         if i % 100 == 0:
             print(article.text)
-        f = open(list[i][1], 'w')
+        f = open('text' + str(i), 'w')
         f.write(article.text)
         f.close()
         i += 1
@@ -149,8 +151,9 @@ intermediate_directory = 'data/'
 def text_open():
     i = 0
     string_list = []
-    strings = ""
-    while i < 3:
+
+    while i < 5:
+        strings = ""
         with open('data/text' + str(i), encoding='utf_8') as f:
             for word in f:
                 strings += word
@@ -163,37 +166,62 @@ def text_open():
 def punct_space(token):
     return token.is_punct or token.is_space
 
-def line_review(filename):
-        for review in filename:
-            # print(review)
-            yield review.replace('\\n', '\n')
+def line_review(article_dir, article_to_parse=3):
+    for article_num in range(article_to_parse):
+        cur_article = article_dir + "/text" + str(article_num)
+        with open(cur_article, encoding="utf-8") as f:
+            next(f)
+            for line in f:
+                yield line.replace('\\n', '\n')
 
-def lemmatized_sentence_corpus(filename):
-    for parsed_review in nlp.pipe(line_review(filename), batch_size=1, n_threads=4):
+
+def lemmatized_sentence_corpus(list_text):
+    for parsed_review in nlp.pipe(line_review(list_text), batch_size=10000, n_threads=4):
+        #print(parsed_review)
         for sent in parsed_review.sents:
             yield u' '.join([token.lemma_ for token in sent if not punct_space(token)])
 
 unigram_sentences_filepath = intermediate_directory + 'unigram_sentences_all.txt'
 
-def runners_Sen(unigram_sentences_filepaths):
 
-    with open(unigram_sentences_filepaths, 'w', encoding='utf_8') as f:
-        for sentence in lemmatized_sentence_corpus(text_open()):
-            #print(sentence)
+def sentence_generator(self, article_dir, articles_to_parse=10000):
+    """
+    Generator function that yields each sentence in all text files.
+    """
+    for article_num in range(articles_to_parse):
+        cur_article = article_dir + "/text" + str(article_num)
+        with open(cur_article, encoding="utf-8") as f:
+            next(f)  # skip first line.
+            data = f.read()
+            corpus = self.nlp(data)
+            for sent in corpus.sents:
+                # filter out punctuation and whitespace from sentences.
+                cur_sentence = " ".join([token.lemma_ for token in sent
+                                         if not self.punct_space(token)])
+                # TODO - Deal with the -PRON-
+                yield cur_sentence
+
+def write_all_article_sentences(self):
+    """
+    writes all sentences into one file.
+    So it can be used by spaCy's LineSentence function.
+    Then returns a LineSentence iterator of sentence unigrams.
+    """
+
+    with open(self.unigram_sentences_filepath, 'w', encoding="utf-8") as f:
+        for sentence in self.sentence_generator("data/"):
             f.write(sentence + '\n')
 
-    unigram_sentences = LineSentence(unigram_sentences_filepaths)
 
     # for unigram_sentence in unigram_sentences:
         # print(u' '.join(unigram_sentence))
         # print(u' ')
 
-    return unigram_sentences
 
 bigram_model_filepath = intermediate_directory + 'bigram_model_all'
 
 def phrases():
-    unigram_sentences = runners_Sen(unigram_sentences_filepath)
+    unigram_sentences = LineSentence(unigram_sentences_filepath)
     bigram_model = Phrases(unigram_sentences)
     bigram_model.save(bigram_model_filepath)
     bigram_model = Phrases.load(bigram_model_filepath)
@@ -201,7 +229,7 @@ def phrases():
     with open(bigram_sentences_filepath, 'w', encoding='utf_8') as f:
         for unigram_sentence in unigram_sentences:
             bigram_sentence = u' '.join(bigram_model[unigram_sentence])
-            f.write(bigram_sentence + '\n')
+            f.write(bigram_sentence )
 
     bigram_sentences = LineSentence(bigram_sentences_filepath)
     # print(u' '.join(bigram_sentence))
@@ -216,7 +244,7 @@ def phrases():
 
     with open(trigram_sentences_filepath, 'w', encoding='utf_8') as f:
         for bigram_sentence in bigram_sentences:
-            trigram_sentence = u' '.join(trigram_model[bigram_sentence])
+            trigram_sentence = ' '.join(trigram_model[bigram_sentence])
             f.write(trigram_sentence + '\n')
 
     trigram_sentences = LineSentence(trigram_sentences_filepath)
@@ -229,7 +257,7 @@ def phrases():
 
     trigram_reviews_filepath = intermediate_directory + 'trigram_transformed_reviews_all.txt'
     with open(trigram_reviews_filepath, 'w', encoding='utf_8') as f:
-        for parsed_review in nlp.pipe(line_review(text_open()),
+        for parsed_review in nlp.pipe(line_review('data/'),
                                       batch_size=10000, n_threads=4):
             # lemmatize the text, removing punctuation and whitespace
             unigram_review = [token.lemma_ for token in parsed_review
@@ -247,7 +275,10 @@ def phrases():
             trigram_review = [term for term in trigram_review if term not in all_stopwords_gensim]
 
             # write the transformed review as a line in the new file
-            trigram_review = u' '.join(trigram_review)
+            trigram_review = ' '.join(trigram_review)
+
+        ## MOVED OUTSIDE OF THE LOOP SO WE COULD GET A SINGULAR CORPUS WITH ALL THE TEXT
+        #print(trigram_review)
             f.write(trigram_review + '\n')
 
     ######BAG OF WORDS CREATION #########
@@ -260,10 +291,14 @@ def phrases():
     # filter tokens that are very rare or too common from
     # the dictionary (filter_extremes) and reassign integer ids (compactify)
     #########trigram_dictionary.filter_extremes(no_below=10, no_above=0.4)##########
-    trigram_dictionary.compactify()
     #print(trigram_dictionary)
 
+    trigram_dictionary.filter_extremes(no_below=0, no_above=100000)
+    trigram_dictionary.compactify()
+    # print(trigram_dictionary)
+
     trigram_dictionary.save(trigram_dictionary_filepath)
+
 
     trigram_dictionary = Dictionary.load(trigram_dictionary_filepath)
 
@@ -271,9 +306,8 @@ def phrases():
     return trigram_dictionary
 
 trigram_bow_filepath = intermediate_directory + 'trigram_bow_corpus_all.mm'
-trigram_dictionary = phrases()
 
-def trigram_bow_generator(filepath):
+def trigram_bow_generator(filepath, trigram_dict):
     """
     generator function to read reviews from a file
     and yield a bag-of-words representation
@@ -281,21 +315,21 @@ def trigram_bow_generator(filepath):
 
 
     for review in LineSentence(filepath):
-        yield trigram_dictionary.doc2bow(review)
+        yield trigram_dict.doc2bow(review)
 
 trigram_reviews_filepath = intermediate_directory + 'trigram_transformed_reviews_all.txt'
 
-def B_O_wCreator(Trigrams_filepath):
+def B_O_wCreator(Trigrams_filepath, trigram_dict):
 
     MmCorpus.serialize(trigram_bow_filepath,
-                       trigram_bow_generator(Trigrams_filepath))
+                       trigram_bow_generator(Trigrams_filepath, trigram_dict))
 
     trigram_bow_corpus = MmCorpus(trigram_bow_filepath)
     return trigram_bow_corpus
 
 def trigramz():
-
-    trigram_bow_corpus = B_O_wCreator(trigram_reviews_filepath)
+    trigram_dictionary = phrases()
+    trigram_bow_corpus = B_O_wCreator(trigram_reviews_filepath, trigram_dictionary)
 
     lda_model_filepath = intermediate_directory + 'lda_model_all'
 
@@ -341,12 +375,13 @@ def explore_topic(topic_number, topn=25):
 LDAvis_data_filepath = intermediate_directory + 'ldavis_prepared'
 
 def LDA_Diagram():
+
     trigram_bow_corpus = MmCorpus(trigram_bow_filepath)
 
     lda = trigramz()
 
     LDAvis_prepared = pyLDAvis.gensim.prepare(lda, trigram_bow_corpus,
-                                              trigram_dictionary)
+                                              phrases())
 
     #print(LDAvis_prepared)
     with open(LDAvis_data_filepath, 'wb') as f:
@@ -361,23 +396,107 @@ def LDA_Diagram():
 
     print(pyLDAvis.display(LDAvis_prepared).data)
 
+
+#--------------------------------------------------- TASK #2 -----------  YEAR OF ARTICLE AND VISUALIZATION ----------------------------------------#
+
+
 Docs = 'black-lives-matter-all-story-urls-20200621214756.csv'
 
-def text_reader(DOC):
+def article_reader(list):
+    i = 0
+    while i < 10:
+        url = list[i][0]
+        article = Article(url, language='en')
+        article.download()
+        try:
+            article.parse()
+        except (ArticleException, AttributeError, UnicodeError) as e:
+            i += 1
+            continue
+        if i % 100 == 0:
+            print(article.text)
+        f = open('text' + str(i), 'w')
+        f.write(article.text)
+        f.close()
+        i += 1
+        time.sleep(5)
+
+def text_reader_Years(DOC):
     with open(DOC) as csv_file:
         list_rows = []
-        csv_reader = csv.reader(csv_file, delimiter=',')
+        csv_reader = list(csv.reader(csv_file, delimiter=','))
         line_count = 0
+        random.shuffle(csv_reader)
         for row in csv_reader:
             if line_count == 0:
                 line_count += 1
             else:
-                list_rows += {(row[1], row[2])}
+                list_rows += {(row[1], row[3])}
                 line_count += 1
         #print(f'Processed {line_count} lines.')
+
+            ## ONLY USING THE FIRST 10
+            if line_count == 11:
+                return list_rows
         return list_rows
 
+def article_read_urls(list):
+    i = 0
+    while i < 10:
+        print(list[i][1])
+        print(list[i][0])
+        url = list[i][1]
+        article = Article(url, language='en')
+        article.download()
+        try:
+            article.parse()
+        except (ArticleException, AttributeError, UnicodeError) as e:
+            i += 1
+            continue
+        if i % 100 == 0:
+            print(article.text)
+        year = list[i][0]
+
+        print(year)
+        f = open('data/' + str(year[:4]) + '/text' + str(i), 'w')
+        f.write(article.text)
+        f.close()
+        i += 1
+        time.sleep(5)
+
+def list_taker(text_list):
+    for url, date in text_list:
+        i = 0
+        string_list = []
+        strings = ""
+        for word in text:
+            strings += word
+
+        strings += "ll" +  date
+        print(strings)
+        # def text_open():
+        #     i = 0
+        #     string_list = []
+        #     strings = ""
+        #     while i < 3:
+        #         with open('data/text' + str(i), encoding='utf_8') as f:
+        #             for word in f:
+        #                 strings += word
+        #             string_list.append(strings)
+        #             # print(string_list)
+        #             # print('\n')
+        #         i += 1
+        #     return string_list
 
 if __name__ == '__main__':
-    print(trigramz().get_document_topics(B_O_wCreator(trigram_reviews_filepath)))
-    print(B_O_wCreator(trigram_reviews_filepath))
+    # v = text_reader_Years('black-lives-matter-all-story-urls-20200621214756.csv')
+    # print(v)
+    # article_read_urls(v)
+
+    # print(len(list(line_review(text_open()))))
+    a = trigramz().get_document_topics(B_O_wCreator(trigram_reviews_filepath, phrases()))
+    for v in a:
+        print(v)
+    print(B_O_wCreator(trigram_reviews_filepath, phrases()))
+
+    #article_reader(text_reader())
